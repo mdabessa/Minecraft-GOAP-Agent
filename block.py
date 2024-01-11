e4abc52328ffe8008d5dc5861d31b30b3b1acae5
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2024-01-10 22:06:24 -0300

    Update block.py

--- block.py
+++ block.py
@@ -11,6 +11,9 @@ require('__all__', globals())
 
 reach = Player.getReach()
 block = Player.rayTraceBlock(reach, True)
+pitch = Player.getPlayer().getPitch()
+yaw = Player.getPlayer().getYaw()
+
 
 if block is None:
     pos = Player.getPlayer().getPos()
@@ -22,11 +25,16 @@ else:
 block = Block.getBlock(pos)
 # block = block.BlockDataHelper()
 
+Chat.log('=== Block ===')
 Chat.log(block.id)
 Chat.log(block.pos)
 Chat.log(f'isAir: {block.isAir}')
 Chat.log(f'isLiquid: {block.isLiquid}')
 Chat.log(f'isOpaque: {block.isOpaque}')
+
+Chat.log('=== Player ===')
+Chat.log('Pitch: ' + str(pitch))
+Chat.log('Yaw: ' + str(yaw))
 
 # Chat.log(block.getBlockStateHelper().isOpaque())
 
