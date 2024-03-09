f5291684db9cad47479a9859a950753f7562c748
Matheus Bessa <mdabessa@gmail.com>
2024-03-09 03:12:30 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,7 +2,12 @@ if __name__ == '':
     from JsMacrosAC import *
 
 
-if event.message.startswith('!') and len(event.message) > 1:
+text = event.text.getString()
+text = text.split('> ')[1]
+
+Chat.log('Processing chat message: ' + text)
+if text.startswith('!') and len(text) > 1:
+    Chat.log('Detected command')
     fakeEvent = JsMacros.createCustomEvent('command')
-    fakeEvent.putString('message', event.message[1:])
+    fakeEvent.putString('message', text[1:])
     JsMacros.runScript('commandHandler.py', fakeEvent)
