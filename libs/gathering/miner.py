f78c2eb7d06dd414ff6542ed816650cd37979621
Matheus Bessa <mdabessa@gmail.com>
2025-02-16 13:01:06 -0300

    Add temporary mining method

--- libs/gathering/miner.py
+++ libs/gathering/miner.py
@@ -5,8 +5,10 @@ if __name__ == '':
     from JsMacrosAC import *
     from libs.utils.calc import Calc, Region
     from libs.utils.dictionary import Dictionary
-    from libs.walk import Walk
-    from libs.actions import Action
+    from libs.utils.logger import Logger
+    from libs.scripts import Script
+    from libs.walk import Walk, Block
+    from libs.actions import Action, NotEnoughItemsError
     from libs.state import State, Waypoint
     from libs.inventory import Inv
     from libs.craft import Craft
@@ -178,9 +180,105 @@ class Miner:
     @staticmethod
     def checkPickaxeLevel(id: str) -> bool:
         """Check the pickaxe level for a block"""
-        actualLevel = Inv.getToolLevel('pickaxe')
+        actualLevel = Inv.getActualToolLevel('pickaxe')
         level = Miner.searchBlockOrItem(id)
         if level == None: return False
         return actualLevel >= level['level']
+
+
+    @staticmethod
+    def mine():
+        # TODO: Temporary mine gathering
+        stopBlocks = ['minecraft:diamond_ore', 'minecraft:deepslate_diamond_ore',
+                    'minecraft:emerald_ore', 'minecraft:deepslate_emerald_ore'
+                    'minecraft:iron_ore', 'minecraft:deepslate_iron_ore',
+                    'minecraft:gold_ore', 'minecraft:deepslate_gold_ore',
+                      ]
+        
+        block = Block.getLookAtBlock()
+        pos = Player.getPlayer().getPos()
+        pos = [math.floor(pos.x), math.floor(pos.y), math.floor(pos.z)]
+        direction = block.pos[0] - pos[0], block.pos[2] - pos[2]
+        if direction[0] == 0 and direction[1] == 0:
+            direction = 0, 1
+        
+        listener = Script.scriptListener('simpleMine')
+        while True:
+            KeyBind.releaseKey('key.keyboard.w')
+            Miner.placeTorch()
+            listener()
+            KeyBind.pressKey('key.keyboard.w')
+
+            if not Miner.checkPickaxeLevel('minecraft:diamond_ore'):
+                raise Exception('Pickaxe level is not enough')
+            
+            dur = Inv.getToolDurability('pickaxe')
+            if dur < 50:
+                raise Exception('Pickaxe is about to break')
+
+            block = Block.getLookAtBlock()
+            if block is not None:
+                pos = Player.getPlayer().getPos()
+                pos = [math.floor(pos.x), math.floor(pos.y), math.floor(pos.z)]
+                
+                while True: # Gravel/sand
+                    listener()
+                    nextPos1 = [pos[0] + direction[0], pos[1]+1, pos[2] + direction[1]]
+                    nextPos2 = [pos[0] + direction[0], pos[1]+2, pos[2] + direction[1]]
+                    block1 = Block.getBlock(nextPos1)
+                    block2 = Block.getBlock(nextPos2)
+
+                    Action.breakBlock(nextPos1, safe=True)            
+                    
+                    if block1.id in ['minecraft:gravel', 'minecraft:sand'] or \
+                        block2.id in ['minecraft:gravel', 'minecraft:sand']:
+                        
+                        Client.waitTick(20)
+                        KeyBind.releaseKey('key.keyboard.w')
+
+                    
+                    block1 = Block.getBlock(nextPos1)
+
+                    if block1 is not None and block1.isLiquid:
+                        KeyBind.releaseKey('key.keyboard.w')
+                        KeyBind.pressKey('key.keyboard.s') # go back
+                        raise Exception('Liquid found')
+
+                    if block1 is None or not block1.isSolid:
+                        break
+                
+                KeyBind.pressKey('key.keyboard.w')
+                nextPos1 = [pos[0] + direction[0], pos[1], pos[2] + direction[1]]
+                Action.breakBlock(nextPos1, safe=True)
+
+                nextPos3 = [pos[0] + direction[0], pos[1]-1, pos[2] + direction[1]]
+                block = Block.getBlock(nextPos3)
+                if block is None or not block.isSolid:
+                    KeyBind.releaseKey('key.keyboard.w')
+                    raise Exception('Block is not solid')
+
+                for x in (-1, 1):
+                    for y in (0, 1):
+                        for z in (-1, 1):
+                            blocksPos = [pos[0] + x, pos[1] + y, pos[2] + z]
+                            block = Block.getBlock(blocksPos)
+                            if block is not None and block.id in stopBlocks:
+                                KeyBind.releaseKey('key.keyboard.w')
+                                Logger.print(f'Goal block found: {block.id}')
+                                return
+                
+                blockPos = [pos[0], pos[1]-1, pos[2]]
+                block = Block.getBlock(blockPos)
+                if block is None or block.id in stopBlocks:
+                    KeyBind.releaseKey('key.keyboard.w')
+                    Logger.print(f'Goal block found: {block.id}')
+                    return
+                
+                blockPos = [pos[0], pos[1]+2, pos[2]]
+                block = Block.getBlock(blockPos)
+                if block is not None and block.id in stopBlocks:
+                    KeyBind.releaseKey('key.keyboard.w')
+                    Logger.print(f'Goal block found: {block.id}')
+                    return
 
 
